e0472a9a578c22de7850d9199791b210a3b32635
benjamin lin <benjamin.lin@us.abb.com>
2020-02-21 12:56:07 -0500

    added api methods

--- src/api/Main.java
+++ src/api/Main.java
@@ -1,10 +1,55 @@
 package api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static HttpURLConnection connection;
 
+	public static void main(String[] args) {
+		// Method 1:java.net.HttpURLConnection
+		
+		BufferedReader reader;
+		String line;
+		StringBuffer responseContent = new StringBuffer();
+		try {
+			URL url = new URL("https://jsonplaceholder.typicode.com/albums");
+			connection = (HttpURLConnection) url.openConnection();
+
+			// request setup
+			connection.setRequestMethod("GET");
+			connection.setConnectTimeout(5000);// 5 seconds
+			connection.setReadTimeout(5000);
+			
+			int status = connection.getResponseCode();
+			System.out.println(status);
+			
+			if(status>299) {
+				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+				//get error message and still have things to read
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+				//if connection is successful
+			}else {
+				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+				while((line = reader.readLine()) != null) {
+					responseContent.append(line);
+				}
+				reader.close();
+			}
+			System.out.println(responseContent.toString());
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
