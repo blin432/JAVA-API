1f5fa7d388f9137b4ce2ab7f8a9b8095b5f8beb1
benjamin lin <benjamin.lin@us.abb.com>
2020-02-21 12:57:18 -0500

    added parse

--- src/api/Main.java
+++ src/api/Main.java
@@ -5,14 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import jdk.incubator.http.HttpClient;
+import jdk.incubator.http.HttpRequest;
+import jdk.incubator.http.HttpResponse;
 
 public class Main {
 
 	private static HttpURLConnection connection;
 
 	public static void main(String[] args) {
-		// Method 1:java.net.HttpURLConnection
+		// Method 1:java.net.HttpURLConnection is sync
 		
 		BufferedReader reader;
 		String line;
@@ -45,11 +53,39 @@ public class Main {
 				reader.close();
 			}
 			System.out.println(responseContent.toString());
+			
+			//parsing data
+			parse(responseContent.toString());
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally {
+			connection.disconnect();
+		}
+		
+		//Method2 : java.net.http.HttpClient will handle async for you//  java 11 feature
+		
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://jsonplaceholder.typicode.com/albums")).build();
+		client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+			.thenApply(HttpResponse:: body)//colon colon is a lambda expresion
+			.thenAccept(System.out::pintln)//.thenApply(Main::parse)
+			.join();
+		
+		// method to parse JSON data//need to parse data
+		//https://mvnrepository.com/artifact/org.json/json
+		public static String parse (String responseBody) {
+			JSONArray albums = new JSONArray (responseBody);
+			for (int i = 0; i<albums.length(); i++) {
+				JSONObject album = albums.getJSONObject(i);
+				int id = albums.getInt("id");
+				int userId = album.getInt("userId");
+				String title = album.getString("title");
+				System.out.println(id + " " + title + " "+ userId);
+			}
+			return null;
 		}
 	}
-
 }
